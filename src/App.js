52b69b93135b5e50db87b7b86652620e0db6c65b
BrentAGit <r0787709@student.thomasmore.be>
2021-06-08 17:52:30 +0200

    Deel1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react"
+
 import './App.css';
 
 function App() {
+    const [books, setBooks] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    console.log("rendering App()...");
+
+    useEffect(() => {
+        console.log("Start of useEffect...");
+
+        async function getBooks() {
+            console.log("Start of async getBooks...");
+            setIsLoading(true);
+            const response = await fetch("http://localhost:8080/books");
+            const body = await response.json();
+            console.log("Received response in getBooks...");
+            setBooks(body);
+            setIsLoading(false);
+            console.log("getBooks Done!");
+        }
+
+        getBooks();
+        setIsLoading(false);
+        console.log("useEffect: finished getBooks...");
+        console.log("useEffect: Done!");
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {books.map((b) => <p key={b.id}>{b.id}. {b.title} - {b.author} [{b.releaseYear}]</p>)}
+        {isLoading ? <p>Loading data...</p> : false}
     </div>
   );
 }
