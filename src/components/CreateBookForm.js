9ae70135f082962657a8dbf234b59df5d85a5e99
BrentAGit <r0787709@student.thomasmore.be>
2021-06-09 22:53:12 +0200

    Made frontend app display error message

--- src/components/CreateBookForm.js
+++ src/components/CreateBookForm.js
@@ -5,6 +5,7 @@ function CreateBookForm(props) {
     const [title, setTitle] = useState();
     const [author, setAuthor] = useState();
     const [releaseYear, setReleaseYear] = useState();
+    const [errorMessage, setErrorMessage] = useState();
 
     async function createBook(book){
         console.log("createBook");
@@ -19,13 +20,22 @@ function CreateBookForm(props) {
         };
         const response = await fetch ("http://localhost:8080/books", fetchOptions);
         const body = await response.json();
-        props.addBook(body);
+        if(response.ok){
+            console.log(`createBook: received response ${JSON.stringify(body)}`);
+            props.addBook(body);
+            console.log(`createBook: done!`);
+        }
+        else{
+            console.log(`createBook: ERROR: ${response.status} - ${body.error} - ${body.message}`);
+            setErrorMessage(body.message)
+        }
         props.setIsLoading(false);
     }
 
     return (
         <div style={{paddingTop: "50px"}}>
             <b>Add a new book to the list</b>
+            <p>{errorMessage}</p>
             <div style={{paddingTop: "10px"}}>
                 Title of the book: <br/>
                 <input value={title} onChange={(e) => setTitle(e.target.value)}/>
